abef2f8bec0c5b53d513cdd5935780c6d86f3b1f
Pedro Alves <140030913+pedroca46@users.noreply.github.com>
2024-09-04 19:44:06 -0300

    JQUERY

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    
+    var nr_c = "";
+
+    $('.bt-calc').click(function(){
+        var nr = $(this).text();
+        nr_c = nr_c.concat(nr)
+
+        $('#txt_display').text(nr_c)
+    })
 });
 
     

5b1ac3e7b778391d6aaa7d81aefafc2b0a2af565
pedroca46 <140030913+pedroca46@users.noreply.github.com>
2024-09-16 19:48:27 -0300

    Aula 16/09 : 19:48

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     
-    $('.bt-calc').click(function(){
-        var bt_nr = $(this).text();
-        $('#txt_display').val(bt_nr)
-    
-   })
 
+    var bt_nr = '';
+
+    $('.bt-calc').click(function() {
+        var bt_nr = $(this).text();
+
+        $('#txt_display').val(bt_nr);
+
+    })     
 });

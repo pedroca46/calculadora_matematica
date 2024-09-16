7ca606279cc827272c98a0c224120cae2b25efff
pedroca46 <140030913+pedroca46@users.noreply.github.com>
2024-09-16 19:57:42 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
     var bt_nr = '';
 
     $('.bt-calc').click(function() {
+        var n = $(this).text()
         var bt_nr = $(this).text();
 
         $('#txt_display').val(bt_nr);
 
-    })     
+    })
 });
